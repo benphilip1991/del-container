73d09a8c5a3d212f9fd0f070e752057e4a4d85e7
Ben Philip <benphilip1991@gmail.com>
2020-02-28 17:14:12 +1100

    Location service cleanup.

--- app/src/main/java/com/del/delcontainer/services/LocationService.java
+++ app/src/main/java/com/del/delcontainer/services/LocationService.java
@@ -5,21 +5,11 @@ import android.location.Location;
 import android.os.Looper;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.SettingsClient;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
-import java.util.ArrayList;
 
 /**
  * Handle location requests from applications.
@@ -31,59 +21,32 @@ public class LocationService {
 
     private Context context;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private ArrayList<Location> locationHistory = new ArrayList<>();
     private boolean gettingLocationUpdates = false;
     private boolean isLiveLocationEnabled = false;
-
     private static LocationService instance = null;
+    private Location lastLocation = null;
 
     private LocationService() {
-        ;
     }
 
     public static synchronized LocationService getInstance() {
         if (null == instance) {
             instance = new LocationService();
         }
-
         return instance;
     }
 
-    // Context dependency injection
     public void initLocationService(Context context) {
-
-        if (null == context) {
+        if (null != context) {
             this.context = context;
         }
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        fusedLocationProviderClient = LocationServices.
+                getFusedLocationProviderClient(this.context);
     }
 
     public Location getLastLocation() {
-
         startLocationUpdates();
-
-        Log.d(TAG, "getLastLocation: called");
-        fusedLocationProviderClient.getLastLocation()
-                .addOnSuccessListener((location) -> {
-                    if (null != location) {
-                        if (0 != locationHistory.size()) {
-                            if (locationHistory.get(locationHistory.size() - 1)
-                                    .getLatitude() != location.getLatitude()
-                                    && locationHistory.get(locationHistory.size() - 1)
-                                    .getLongitude() != location.getLongitude()) {
-
-                                Log.d(TAG, "onSuccess: Adding new location : "
-                                        + location.getLatitude() + " | " + location.getLongitude());
-                                locationHistory.add(location);
-                            }
-                        } else {
-                            locationHistory.add(location);
-                        }
-                    }
-                });
-
-        return locationHistory.get(locationHistory.size() - 1);
+        return lastLocation;
     }
 
     /**
@@ -91,38 +54,11 @@ public class LocationService {
      * Else, switch to false.
      * Can maintain a list of apps requesting location. -> do later
      *
-     * @param flag
+     * @param flag true to enable location
      */
     public void setLocationServiceEnabled(boolean flag) {
         isLiveLocationEnabled = flag;
     }
-
-    /**
-     * Method to get location and push to calling app.
-     */
-    private void getLocation() {
-
-        // Connect to the google play services and the location services API
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(locationRequest);
-
-        SettingsClient settingsClient = LocationServices.getSettingsClient(context.getApplicationContext());
-        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
-
-        task.addOnSuccessListener((locationSettingsResponse) -> {
-            ;
-        });
-
-        task.addOnFailureListener((e) -> {
-            e.printStackTrace();
-        });
-    }
-
 
     /**
      * Start location updates
@@ -131,7 +67,6 @@ public class LocationService {
 
         // If location is already being fetched, ignore
         if (!gettingLocationUpdates && isLiveLocationEnabled) {
-
             gettingLocationUpdates = true;
             LocationRequest locationRequest = LocationRequest.create();
             locationRequest.setInterval(2000);
@@ -148,7 +83,6 @@ public class LocationService {
      * Stop location updates. Apps can use the last update.
      */
     public void stopLocationUpdates() {
-
         if (gettingLocationUpdates) {
             gettingLocationUpdates = false;
             isLiveLocationEnabled = false;
@@ -156,20 +90,18 @@ public class LocationService {
         }
     }
 
+    /**
+     * Update the last location object
+     */
     private LocationCallback locationCallback = new LocationCallback() {
-
         @Override
         public void onLocationResult(LocationResult locationResult) {
-            if (null == locationResult) {
-                return;
-            }
-
-            // Push data to app
-            for (Location location : locationResult.getLocations()) {
-                Log.d(TAG, "onLocationResult: Latitude : " + location.getLatitude()
-                        + " | Longitude : " + location.getLongitude() + " | Accuracy : " + location.getAccuracy());
+            if (null != locationResult) {
+                lastLocation = locationResult.getLastLocation();
+                Log.d(TAG, "onLocationResult: Latitude : " + lastLocation.getLatitude()
+                + " | Longitude : " + lastLocation.getLongitude()
+                + " | Accuracy : " + lastLocation.getAccuracy());
             }
         }
     };
-
 }
