a08332e6ba88acddc664739855080b46c28accba
Trevor Neil Loren <trevorloren@gmail.com>
2020-09-30 13:05:58 +1000

    Added function for date filtering logs; fixed bugs
    - Added a function to to filter sensor records by data
    - Fixed storage creation issue for app-specific storage
    - Reorganised code and added comments

--- app/src/main/java/com/del/delcontainer/managers/DataManager.java
+++ app/src/main/java/com/del/delcontainer/managers/DataManager.java
@@ -20,10 +20,14 @@ import com.google.gson.Gson;
 
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Data handler to manage data storage/retrieval from
@@ -35,23 +39,26 @@ import java.util.List;
 public class DataManager {
     private static final String TAG = "DataManager";
 
-    // TODO: Need a scheduler to go through the mapped requests and deliver -> DataProviderService
-    // Map of callback requests resource -> dictionary of (appId -> callback),
+    // Map of callback requests resource -> dictionary of (appId -> callback)
     static HashMap<String, HashMap<String, String>> dataRequestMap;
 
-    // Map of logger requests resource -> number of apps requiring data,
+    // Map of logger requests resource -> number of apps requiring data
     static HashMap<String, Integer> sensorLoggerFlags;
 
     //Object to store installed apps and permissions
     private static CustomMutableLiveData<UserServicesRepository> userServicesRepository =
             new CustomMutableLiveData<>();
 
+    //Singleton instance of the DEL DataManager
     private static DataManager dataManager;
 
+    //Object for app-specific storage
     private static FileStorage fileStorage;
 
+    //Object for sensor log retrieval
     private static SensorRecordDao sensorRecordDao;
 
+    //List of linked applications including permissions
     private static ArrayList<LinkedApplicationDetails> linkedApps;
 
     private DataManager(Context context) {
@@ -62,13 +69,12 @@ public class DataManager {
         DelDatabase database = DelDatabase.getInstance(context);
         sensorRecordDao = database.sensorRecordDao();
 
-        //TODO: Observe and update installed app list when changes are triggered
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
                 userServicesRepository.observeForever((userServicesRepository) -> {
                     Log.d(TAG, "Observing for changes");
-                    //Run commands that are triggered when application lists changes
+                    //Running commands that are triggered when the application list changes
                     linkedApps = userServicesRepository.getUserServicesList();
                 });
             }
@@ -76,7 +82,7 @@ public class DataManager {
     }
 
     public static void initDataManager(Context context) {
-        if(null == dataManager){
+        if (null == dataManager) {
             dataManager = new DataManager(context);
         }
     }
@@ -85,20 +91,9 @@ public class DataManager {
         return dataManager;
     }
 
-    public static String getAppData(String appId) {
-        return fileStorage.ReadFile(appId);
-    }
-
-    public static void setAppData(String appId, String content) {
-        fileStorage.WriteFile(appId, content);
-    }
-
-    public static boolean getLoggerRequestFlag(String resource) {
-        return sensorLoggerFlags.get(resource) != null && sensorLoggerFlags.get(resource) > 0;
-    }
-
     /**
-     * Method to delete resource requests associated to an app
+     * Method to delete callback resource requests associated to an app
+     *
      * @param resource
      * @param appId
      */
@@ -107,7 +102,8 @@ public class DataManager {
     }
 
     /**
-     * Method to get all requests for a resource
+     * Method to get all callback requests for a resource
+     *
      * @param resource
      */
     public HashMap<String, String> getCallBackRequests(String resource) {
@@ -115,17 +111,45 @@ public class DataManager {
     }
 
     /**
+     * Method to check if data from a resource needs to be logged
+     *
+     * @param resource
+     */
+    public static boolean getLoggerRequestFlag(String resource) {
+        return sensorLoggerFlags.get(resource) != null && sensorLoggerFlags.get(resource) > 0;
+    }
+
+    /**
+     * Method to get app-specific storage JSON strings
+     *
+     * @param appId
+     */
+    public static String getAppData(String appId) {
+        return fileStorage.ReadFile(appId);
+    }
+
+    /**
+     * Method to save app-specific storage JSON strings
+     *
+     * @param appId
+     */
+    public static void setAppData(String appId, String content) {
+        fileStorage.WriteFile(appId, content);
+    }
+
+    /**
      * Method to set application callback requests for a resource
+     *
      * @param appId
      * @param requests
      */
     public static void setCallBackRequests(String appId, ArrayList<JSONObject> requests) {
-        try{
-            for (JSONObject request:requests) {
+        try {
+            for (JSONObject request : requests) {
                 String resource = request.getString(Constants.RESOURCE);
                 String callback = request.getString(Constants.CALLBACK);
-                if(validatePermissions(appId, resource)){
-                    if(null == dataRequestMap.get(resource)) {
+                if (validatePermissions(appId, resource)) {
+                    if (null == dataRequestMap.get(resource)) {
                         dataRequestMap.put(resource, new HashMap<String, String>());
                     }
                     Log.d(TAG, "setCallBackRequests: Setting" + resource
@@ -139,36 +163,36 @@ public class DataManager {
                 }
             }
         } catch (Exception e) {
-            Log.d(TAG,"setCallBackRequests: Error while parsing callback requests");
+            Log.d(TAG, "setCallBackRequests: Error while parsing callback requests");
         }
     }
 
     /**
-     * Method to set application data logging requests for a resource
+     * Method to set/remove application data logging requests for a resource
+     *
      * @param appId
      * @param requests
      */
     public static void setSensorLoggerRequests(String appId, ArrayList<JSONObject> requests) {
         try {
-            for (JSONObject request:requests) {
+            for (JSONObject request : requests) {
                 String resource = request.getString(Constants.RESOURCE);
                 boolean toggle = request.getBoolean(Constants.TOGGLE);
-                if(validatePermissions(appId, resource)){
+                if (validatePermissions(appId, resource)) {
                     Integer curFlag;
-                    if(!sensorLoggerFlags.containsKey(resource))
+                    if (!sensorLoggerFlags.containsKey(resource))
                         curFlag = 0;
                     else
                         curFlag = sensorLoggerFlags.get(resource);
 
-                    if(!toggle && curFlag>0) {
+                    if (!toggle && curFlag > 0) {
                         Log.d(TAG, "setSensorLoggerRequests: Removing " + resource
                                 + " logger request for service : " + appId);
-                        sensorLoggerFlags.put(resource, curFlag-1);
-                    }
-                    else {
+                        sensorLoggerFlags.put(resource, curFlag - 1);
+                    } else {
                         Log.d(TAG, "setSensorLoggerRequests: Setting " + resource
                                 + " logger request for service : " + appId);
-                        sensorLoggerFlags.put(resource, curFlag+1);
+                        sensorLoggerFlags.put(resource, curFlag + 1);
                     }
                 } else {
                     Log.d(TAG, "setSensorLoggerRequests: No permission to set " + resource
@@ -176,26 +200,69 @@ public class DataManager {
                 }
             }
         } catch (Exception e) {
-            Log.d(TAG,"setSensorLoggerRequests: Error while parsing logger requests");
+            Log.d(TAG, "setSensorLoggerRequests: Error while parsing logger requests");
         }
     }
 
     /**
-     * Method to get application data logs for a resource
+     * Method to get all application data logs for a resource
+     *
      * @param appId
      * @param resource
      */
     public static String getSensorLogs(String appId, String resource) {
-        if(validatePermissions(appId, resource)) {
+        if (validatePermissions(appId, resource)) {
             Gson gson = new Gson();
             List<SensorRecord> sensorRecords = sensorRecordDao.getAllSensorRecords(resource);
-            Log.d(TAG,"getSensorLogs: Sending " + resource + " logs to app "+appId);
+            Log.d(TAG, "getSensorLogs: Sending " + resource + " logs to app " + appId);
             return gson.toJson(sensorRecords);
         } else {
-            Log.d(TAG,"getSensorLogs: No permissions to send " + resource
-                    + " logs to app "+appId);
+            Log.d(TAG, "getSensorLogs: No permissions to send " + resource
+                    + " logs to app " + appId);
             return null;
         }
+    }
+
+    /**
+     * Method to get date filtered application data logs for a resource
+     *
+     * @param appId
+     * @param resource
+     * @param start
+     * @param end
+     */
+    public static String getSensorLogs(String appId, String resource, String start, String end) {
+        if (validatePermissions(appId, resource)) {
+            Gson gson = new Gson();
+            try {
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+                format.setTimeZone(TimeZone.getTimeZone("UTC"));
+                Date startDate = format.parse(start);
+                Date endDate = format.parse(end);
+                List<SensorRecord> sensorRecords = sensorRecordDao
+                        .getSensorRecordsBetweenDates(resource, startDate, endDate);
+                Log.d(TAG, "getSensorLogs: Sending " + resource + " logs to app " + appId);
+                return gson.toJson(sensorRecords);
+            } catch (Exception e) {
+                Log.d(TAG, "getSensorLogs: Error while parsing date for " + appId);
+            }
+        } else {
+            Log.d(TAG, "getSensorLogs: No permissions to send " + resource
+                    + " logs to app " + appId);
+        }
+        return null;
+    }
+
+    /**
+     * Logging utility for sensor handlers to store sensor records
+     *
+     * @param sensor
+     * @param reading
+     */
+    public static void LogSensorRecord(String sensor, String reading) {
+        Log.d(TAG, "LogSensorRecord: Logged record for sensor:"+sensor);
+        new SensorEventLoggerTask()
+                .execute(new SensorRecord(new Date(), sensor, reading));
     }
 
     /**
@@ -229,12 +296,9 @@ public class DataManager {
         }
     }
 
-    public static void LogSensorRecord(String sensor, String reading) {
-        Log.d(TAG, "LogSensorRecord: Logged record for sensor:"+sensor);
-        new SensorEventLoggerTask()
-                .execute(new SensorRecord(new Date(), sensor, reading));
-    }
-
+    /**
+     * Async storage of sensor logs
+     */
     private static class SensorEventLoggerTask extends AsyncTask<SensorRecord, Void, Void> {
         @Override
         protected Void doInBackground(SensorRecord... sensorRecord) {
