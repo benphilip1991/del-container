634c3fc6b4e1a18a8f4f33b82a9dcf34de404d61
Ben Philip <benphilip1991@gmail.com>
2021-02-08 11:37:37 +1100

    Optimizing DEL Utils; Refactoring and removing deprecated functions;

--- app/src/main/java/com/del/delcontainer/utils/DELUtils.java
+++ app/src/main/java/com/del/delcontainer/utils/DELUtils.java
@@ -1,29 +1,18 @@
 package com.del.delcontainer.utils;
 
 import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
 import android.os.Build;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
-import android.widget.Toast;
 
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-
-import com.del.delcontainer.database.entities.Heart;
 import com.del.delcontainer.managers.DataManager;
-import com.del.delcontainer.repositories.HeartRateRepository;
-import com.del.delcontainer.services.LocationService;
-import com.del.delcontainer.services.SensorsService;
+import com.del.delcontainer.ui.login.LoginStateRepo;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 /**
  * App APIs exposed to the DEL web applications
@@ -55,8 +44,8 @@ public class DELUtils {
      * Build a function call formatted for the appView as a string
      * Eg: "function_name('param1', 'param2')"
      *
-     * @param targetFunction
-     * @param params
+     * @param targetFunction Function to be called in the mini-apps
+     * @param params Params to be injected
      * @return
      */
     public String getTargetFunctionString(final String targetFunction, final Object[] params) {
@@ -64,11 +53,11 @@ public class DELUtils {
         // Build a function call with parameters as a string
         StringBuilder sb = new StringBuilder(targetFunction).append('(');
         for (int i = 0; i < params.length; i++) {
-            if(params[i] instanceof String) {
+            if (params[i] instanceof String) {
                 sb.append("\'").append(params[i]).append("\'");
             }
 
-            if(i != params.length - 1) {
+            if (i != params.length - 1) {
                 sb.append(",");
             }
         }
@@ -106,47 +95,6 @@ public class DELUtils {
         });
     }
 
-    /**
-     * Inject the registered app ID and app name to the service
-     * when launched. This will be used by the services when terminating
-     * @param appId
-     * @param appName
-     */
-    public void setAppIdAndName(String appId, String appName) {
-
-    }
-
-    @JavascriptInterface
-    public void makeToast(String message) {
-        Log.d(TAG, "makeToast: Called MakeToast");
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-    }
-
-    @JavascriptInterface
-    public void registerApp(String appDetails) {
-
-        UUID appId = null;
-        String appName = null;
-
-        try {
-            JSONObject appInfo = new JSONObject(appDetails);
-            appId = UUID.fromString(appInfo.getString(Constants.APP_ID));
-            appName = appInfo.getString(Constants.APP_NAME);
-
-            Log.d(TAG, "registerApp: " + appName + "; ID : " + appId);
-
-        } catch (JSONException e) {
-            Log.d(TAG, "registerApp: ");
-        }
-
-        // Broadcast app registration
-        Intent intent = new Intent(Constants.EVENT_APP_REGISTERED);
-        intent.putExtra(Constants.APP_ID, appId);
-        intent.putExtra(Constants.APP_NAME, appName);
-
-        Log.d(TAG, "registerApp: sending broadcast");
-        LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
-    }
 
     /**
      * Function to set requests for data callbacks from container
@@ -163,11 +111,11 @@ public class DELUtils {
             JSONArray requests = reqObject.getJSONArray(Constants.APP_REQUESTS);
 
             ArrayList<JSONObject> requestList = new ArrayList<>();
-            for (int i = 0; i < requests.length(); requestList.add(requests.getJSONObject(i++)));
+            for (int i = 0; i < requests.length(); requestList.add(requests.getJSONObject(i++))) ;
 
             dataManager.setCallBackRequests(appId, requestList);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             Log.d(TAG, "setCallbackRequest : " + e.getMessage());
         }
     }
@@ -186,49 +134,53 @@ public class DELUtils {
             JSONArray requests = reqObject.getJSONArray(Constants.APP_REQUESTS);
 
             ArrayList<JSONObject> requestList = new ArrayList<>();
-            for (int i = 0; i < requests.length(); requestList.add(requests.getJSONObject(i++)));
+            for (int i = 0; i < requests.length(); requestList.add(requests.getJSONObject(i++))) ;
 
             DataManager.setSensorLoggerRequests(appId, requestList);
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             Log.d(TAG, "setLoggerRequest : " + e.getMessage());
         }
     }
 
     /**
      * Get app-specific storage
+     *
      * @param AppId
      * @return
      */
     @JavascriptInterface
-    public String getAppData(String AppId){
+    public String getAppData(String AppId) {
         return DataManager.getAppData(AppId);
     }
 
     /**
      * Set app-specific storage
+     *
      * @param AppId
      * @param content
      * @return
      */
     @JavascriptInterface
-    public void setAppData(String AppId, String content){
+    public void setAppData(String AppId, String content) {
         DataManager.setAppData(AppId, content);
     }
 
     /**
-     *Get logged sensor data
+     * Get logged sensor data
+     *
      * @param AppId
      * @param sensor
      * @return
      */
     @JavascriptInterface
-    public String getData(String AppId, String sensor){
+    public String getData(String AppId, String sensor) {
         return DataManager.getSensorLogs(AppId, sensor);
     }
 
     /**
-     *Get logged sensor data filtered by date
+     * Get logged sensor data filtered by date
+     *
      * @param AppId
      * @param sensor
      * @param start
@@ -236,121 +188,12 @@ public class DELUtils {
      * @return
      */
     @JavascriptInterface
-    public String getData(String AppId, String sensor, String start, String end){
+    public String getData(String AppId, String sensor, String start, String end) {
         return DataManager.getSensorLogs(AppId, sensor, start, end);
     }
 
-    /**
-     * Fetch latest HR data only once.
-     * @return
-     */
     @JavascriptInterface
-    public String getLatestHeartData() {
-
-        JSONObject hrData = new JSONObject();
-
-        Heart heartData = HeartRateRepository.getInstance(context.getApplicationContext())
-                .getLatestHeartData();
-        if (null == heartData) {
-            return "No Data";
-        }
-
-        try {
-            hrData.put("timestamp", heartData.getDate());
-            hrData.put("heartRateValue", heartData.getHeartRate());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return hrData.toString();
-    }
-
-    @JavascriptInterface
-    public String getAllHeartRate() {
-
-        JSONArray hrDataArray = new JSONArray();
-        JSONObject hrBlock = new JSONObject();
-
-        List<Heart> heartRateData = HeartRateRepository.getInstance(
-                context.getApplicationContext()).getHeartData();
-
-        if (null == heartRateData) {
-            return "No Data";
-        }
-
-        try {
-            for (Heart heart : heartRateData) {
-                JSONObject heartRateJsonData = new JSONObject();
-                heartRateJsonData.put("timestamp", heart.getDate());
-                heartRateJsonData.put("heartRateValue", heart.getHeartRate());
-                hrDataArray.put(heartRateJsonData);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            hrBlock.put("hrData", hrDataArray);
-        } catch (Exception e) {
-            ;
-        }
-
-        return hrBlock.toString();
-    }
-
-    @JavascriptInterface
-    public String getStepsCount() {
-        return String.valueOf(SensorsService.getInstance().getStepCount());
-    }
-
-    @JavascriptInterface
-    public String getCurrentLocation() {
-
-        JSONObject locationObject = new JSONObject();
-        LocationService locationService = LocationService.getInstance();
-        locationService.setLocationServiceEnabled(true);
-
-        Location location = locationService.getLastLocation();
-
-        if (null != location) {
-            try {
-                locationObject.put("latitude", location.getLatitude());
-                locationObject.put("longitude", location.getLongitude());
-                locationObject.put("accuracy", location.getAccuracy());
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return locationObject.toString();
-    }
-
-    @JavascriptInterface
-    public void stopLocationUpdates() {
-        Log.d(TAG, "stopLocationUpdates: Terminating location updates");
-        LocationService locationService = LocationService.getInstance();
-        locationService.setLocationServiceEnabled(false);
-        locationService.stopLocationUpdates();
-    }
-
-    // Need to add repository interface in each function here
-    @JavascriptInterface
-    public String getLatestWeight() {
-        return "";
-    }
-
-    @JavascriptInterface
-    public String getBodyMassParams() {
-        return "";
-    }
-
-    @JavascriptInterface
-    public String getLatestStepCount() {
-        return "";
-    }
-
-    @JavascriptInterface
-    public String getStepGoals() {
-        return "";
+    public String getUserName() {
+        return LoginStateRepo.getInstance().getFirstName();
     }
 }
